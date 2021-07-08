2829cb0d70c8179b066522428eeae17331b95abe
Oussama El ouarti <oel-ouar@e0r4p2.1337.ma>
2021-07-08 20:07:41 +0100

    ugh

--- ft_containers/Vector/Vector.hpp
+++ ft_containers/Vector/Vector.hpp
@@ -4,12 +4,14 @@
 #include  <exception>
 #include <cstring>
 #include <memory>
+#include "ReverseIterator.hpp"
+#include "type_traits.hpp"
 
-template<typename Vector>
+template<typename T>
 class Vectoriterator
 {
     public :
-        typedef typename Vector::value_type value_type;
+        typedef T value_type;
         typedef value_type* PointerType;
         typedef const value_type * const_PointerType;
         typedef value_type& ReferenceType;
@@ -32,9 +34,9 @@ class Vectoriterator
             m_ptr++;
             return (*this);
         }
-        operator Vectoriterator<Vector const>() const
+        operator Vectoriterator<const value_type>() const
         {
-            return Vectoriterator<Vector const>(m_ptr);
+            return Vectoriterator<const value_type>(m_ptr);
         }
         Vectoriterator operator++(int)
         {
@@ -96,7 +98,7 @@ class Vectoriterator
             return (*this);
         }
     protected :
-        PointerType m_ptr;
+        value_type * m_ptr;
 };
 
 template<typename T, class Alloc = std::allocator<T> >
@@ -108,8 +110,10 @@ class Vector
         typedef const T& const_reference;
         typedef T* pointer;
         typedef const T* const_pointer;
-        typedef Vectoriterator<Vector<T> > iterator;
-        typedef Vectoriterator<Vector<const T> > const_iterator;
+        typedef Vectoriterator<T> iterator;
+        typedef Vectoriterator<const T> const_iterator;
+        typedef Reverse_iterator<iterator> reverse_iterator;
+        typedef Reverse_iterator<const_iterator> const_reverse_iterator;
         typedef Alloc allocator_type;
 
     public :
@@ -121,13 +125,9 @@ class Vector
         }
         Vector(unsigned long long n, T value, const allocator_type& alloc = allocator_type()) : m_data(nullptr), m_size(0), m_capacity(0)
         {
-            if (n > 4611686018427387903)
-                throw OutOfRange();
-            else if (n == 4611686018427387903)
-                throw BadAlloc();
             m_data = m_alloc.allocate(n);
             m_alloc.construct(m_data);
-            for (unsigned int i = 0; i < n; i++)
+            for (unsigned long long i = 0; i < n; i++)
             {
                 m_size = i;
                 m_data[i] = std::move(value);
@@ -135,10 +135,11 @@ class Vector
             m_size++;
             m_capacity = n;
         }
-        Vector(iterator first, iterator last, const allocator_type& alloc = allocator_type()) : m_data(nullptr), m_size(0), m_capacity(0)
+        template< typename Y>
+        Vector(Y first, Y last, const allocator_type& alloc = allocator_type(), typename enable_if<!is_integral<Y>::value,Y >::type = Y()) : m_data(nullptr), m_size(0), m_capacity(0)
         {
             size_t i = 0;
-            for (iterator it = first; it != last; it++)
+            for (Y it = first; it != last; it++)
                 i++;
             m_data = m_alloc.allocate(i + 1);
             m_alloc.construct(m_data);
@@ -165,6 +166,7 @@ class Vector
             m_capacity = x.m_capacity;
             return (*this);
         }
+
         const_reference operator[](size_t index) const
         {
             return (m_data[index]);
@@ -186,7 +188,13 @@ class Vector
         void    pop_back() { m_size--;}
         void    clear(){m_size = 0;}
         iterator begin() {return (iterator(m_data));}
-        iterator end() {return (iterator(m_data + m_size));}
+        iterator end()  {return (iterator(m_data + m_size));}
+        const_iterator begin() const {return (iterator(m_data));}
+        const_iterator end()  const {return (iterator(m_data + m_size));}
+        const_reverse_iterator rbegin() const {return (reverse_iterator(end()));}
+        const_reverse_iterator rend()  const {return (reverse_iterator(begin()));}
+        reverse_iterator rbegin() {return (reverse_iterator(end()));}
+        reverse_iterator rend() {return (reverse_iterator(begin()));}
         size_t  capacity(){ return(m_capacity);}
         size_t  max_size(){ return (m_alloc.max_size());}
         bool    empty()
@@ -227,15 +235,14 @@ class Vector
         iterator insert(iterator position, const T& value)
         {
             int i = 0;
-            int j;
-            for(iterator it = begin(); it != position; it++)
+            int j = 0;
+            for (iterator it = begin(); it != position; it++)
                 i++;
-            i++;
-            m_data = m_alloc.allocate(m_capacity);
-            m_alloc.construct(m_data);
+            if (m_capacity == m_size)
+                reserve(m_capacity * 2);
             for (j = m_size; j >= i; j--)
-                m_data[j] = m_data[j - 1];
-            m_data[j] = std::move(value);
+                m_data[j + 1] = m_data[j];
+            m_data[j + 1] = value;
             m_size++;
             return (position);
         }
@@ -254,7 +261,7 @@ class Vector
         //     for (j = 0; j < i; j++)
         //         m_data[j] = std::move(newBlock[j]);
         //     int l = j;
-        //      for (k = 0; k < n; k++)
+        //     for (k = 0; k < n; k++)
         //     {
         //         m_data[j] = std::move(value);
         //         j++;
@@ -300,31 +307,18 @@ class Vector
         //     }
         //     delete [] newBlock;
         // }
-        // void    resize(size_t n, T value)
-        // {
-        //     if (n > 4611686018427387903)
-        //         throw OutOfRange();
-        //     else if (n == 4611686018427387903)
-        //         throw BadAlloc();
-        //     if (n > m_capacity)
-        //     {
-        //         m_data = m_alloc.allocate(n);
-        //         m_alloc.construct(m_data);
-        //         for (int i = m_size; m_size < n; m_size++)
-        //             m_data[m_size] = std::move(value);
-        //     }
-        //     else
-        //     {
-        //         for (int i = m_size; i > n; i--)
-        //             m_data[m_size--].~T();
-        //     }
-        // }
+        void    resize(size_t n, T value)
+        {
+            if (n > m_size)
+                *this = Vector(n, value);
+            else
+            {
+                for (int i = m_size; i > n; i--)
+                    m_data[m_size--].~T();
+            }
+        }
         void    reserve(size_t n)
         {
-            if (n > m_alloc.max_size())
-                throw OutOfRange();
-            else if (n == m_alloc.max_size())
-                throw BadAlloc();
             if (n > m_capacity)
             {
                 T* block = new T[m_size];
@@ -339,34 +333,21 @@ class Vector
                 delete [] block;
             }
         }
-        iterator    erase(iterator position)
-        {
-            int n = 0;
-            iterator it;
-            for (it = begin(); it != position; it++)
-                n++;
-            m_size--;
-            for(size_t i = n; i < m_size; i++)
-                m_data[i] = std::move(m_data[i + 1]);
-            return (it);
-        }
+        // iterator    erase(iterator position)
+        // {
+        //     m_size--;
+        //     for (iterator it = position; it != end(); it++)
+        //         *it = *it + 1;
+        //     return (position);
+        // }
         // iterator    erase(iterator first, iterator last)
         // {
-        //     size_t n = 0;
-        //     size_t i = 0;
-        //     iterator it;
-        //     for (it = begin(); it != first; it++)
-        //         n++;
-        //     for (iterator it = first; first != last; first++)
-        //         i++;
-        //     i--;
-        //     for (int k = n; k < i + n; k++)
+        //     for (iterator it = first; it != last; it++)
         //     {
-        //         m_data[k].~T();
-        //         m_data[k] = std::move(m_data[k + i]);
+        //         *it = *it + 1;
         //         m_size--;
         //     }
-        //     return (it);
+        //     return (first);
         // }
         void    swap(Vector<T>& x)
         {
@@ -390,7 +371,8 @@ class Vector
 				virtual const char* what() const throw() {return ("bad::alloc");}
 		};
     private :
-        Alloc m_alloc;
+        
+        Alloc   m_alloc;
         T*      m_data;
         size_t  m_size;
         size_t  m_capacity;  
@@ -398,42 +380,22 @@ class Vector
 template<typename T>
 bool operator==(const Vector<T>& lhs, const Vector<T>& rhs)
 {
-    for (size_t i = 0; i < lhs.size(); i++)
-    {
-        if (lhs[i] == rhs[i])
-            continue ;
-        else 
-            return (false);
-    }
-    return (true);
+   return (equal(lhs.begin(), lhs.end(), rhs.begin()));
 }
 template<typename T>
 bool operator!=(const Vector<T>& lhs, const Vector<T>& rhs)
 {
-    if (lhs == rhs)
-        return (false);
-    return (true);
+    return (!(lhs == rhs));
 }
 template<typename T>
 bool operator<(const Vector<T>& lhs, const Vector<T>& rhs)
 {
-    for (size_t i = 0; i < lhs.size(); i++)
-    {
-        if (lhs[i] > rhs[i]) 
-            return (false);
-        else if (lhs[i] < rhs[i])
-            return (true);
-    }
-    return (false);
+    return (lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
 }
 template<typename T>
 bool operator>(const Vector<T>& lhs, const Vector<T>& rhs)
 {
-    if (lhs < rhs || lhs == rhs)
-        return (false);
-    else if (lhs != rhs)
-        return (true);
-    return (false);
+   return (!lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
 }
 template<typename T>
 bool operator<=(const Vector<T>& lhs, const Vector<T>& rhs)
@@ -445,7 +407,7 @@ bool operator<=(const Vector<T>& lhs, const Vector<T>& rhs)
 template<typename T>
 bool operator>=(const Vector<T>& lhs, const Vector<T>& rhs)
 {
-    if (lhs  > rhs || lhs == rhs)
+    if (lhs > rhs || lhs == rhs)
         return (true);
     return (false);
 }
--- ft_containers/Vector/main.cpp
+++ ft_containers/Vector/main.cpp
@@ -4,65 +4,99 @@
 #include <array>
 int main()
 {
-    // constuct with n range && capacity && push && pop back
-    Vector<int> vectorr(10, 100);
-    std::cout << "vectorr ---" << std::endl;
-    vectorr.push_back(4);
-    vectorr.push_back(4);
-    vectorr.pop_back();
-    for (Vector<int>::iterator it = vectorr.begin(); it != vectorr.end(); it++)
-        std::cout << *it << std::endl;
-    std::cout << vectorr.capacity() << std::endl;
-    // construct  with iterators
-    Vector<int>::iterator it = vectorr.begin();
-    it++;
-    it++;
-    it++;
-    Vector<int> vec(vectorr.begin(), it);
-    std::cout << "vec ---" << std::endl;
-    for (Vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
-        std::cout << *it << std::endl;
-    // copy construct
-    Vector<int> vecto(vec);
-    std::cout << "vecto ---" << std::endl;
-    for (Vector<int>::iterator it = vecto.begin(); it != vecto.end(); it++)
-        std::cout << *it << std::endl;
-    // assign && reserve
-    std::cout << "vectorr assign" << std::endl;
-    vectorr.assign(4, 5);
-    vectorr.reserve(100);
-    for (Vector<int>::iterator it = vectorr.begin(); it != vectorr.end(); it++)
-        std::cout << *it << std::endl;
-    std::cout << vectorr.capacity() << std::endl;
-    // swap && erase
-    std::cout << "erase" << std::endl;
+    Vector<int> van(5,10);
+    // std::vector<int> bo(++van.begin(),--van.end());
+    //  if (bo == van)
+    //     std::cout << "kik" << std::endl;
+    // else
+    //     std::cout << "kok" << std::endl;
+    // // constuct with n range && capacity && push && pop back
+    // Vector<int> vectorr(10, 100);
+    // std::cout << "vectorr ---" << std::endl;
+    // vectorr.push_back(4);
+    // vectorr.push_back(4);
+    // vectorr.pop_back();
+    // for (Vector<int>::iterator it = vectorr.begin(); it != vectorr.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::cout << vectorr.capacity() << std::endl;
+    // // construct  with iterators
+    // Vector<int>::iterator it = vectorr.begin();
+    // it++;
+    // it++;
+    // it++;
+    // Vector<int> vec(vectorr.begin(), it);
+    // std::cout << "vec ---" << std::endl;
+    // for (Vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+    //     std::cout << *it << std::endl;
+    // // copy construct
+    // Vector<int> vecto(vec);
+    // std::cout << "vecto ---" << std::endl;
+    // for (Vector<int>::iterator it = vecto.begin(); it != vecto.end(); it++)
+    //     std::cout << *it << std::endl;
+    // // assign && reserve
+    // std::cout << "vectorr assign" << std::endl;
+    // vectorr.assign(4, 5);
+    // vectorr.reserve(100);
+    // for (Vector<int>::iterator it = vectorr.begin(); it != vectorr.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::cout << vectorr.capacity() << std::endl;
+    // // swap && erase
+    // std::cout << "erase" << std::endl;
     Vector<int> va;
     va.push_back(1);
     va.push_back(2);
     va.push_back(3);
-    Vector<int>::iterator to = va.erase(--va.end());
+    va.push_back(4);
+    // Vector<int>::iterator to = va.erase(va.begin());
     // for (Vector<int>::iterator it = va.begin(); it != va.end(); it++)
-        std::cout << *to << std::endl;
-    std::cout << "-----------------------------" << std::endl;
-    std::vector<int> vo(10, 100);
-    vo.push_back(4);
-    vo.push_back(4);
-    vo.pop_back();
-    vo.assign(4, 5);
-    vo.reserve(100);
-    for (std::vector<int>::iterator it = vo.begin(); it != vo.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::cout << *to << std::endl;
+    // // erase range of its
+    // std::cout << "range erase" << std::endl;
+    // Vector<int>::iterator top = va.erase(va.begin(), va.end());
+    // for (Vector<int>::iterator it = va.begin(); it != va.end(); it++)
+    //     std::cout << *it << std::endl;
+    //     std::cout << *to << std::endl;
+    // resize
+    // std::cout << "resize" << std::endl;
+    // va.resize(6,10);
+    // for (Vector<int>::iterator it = va.begin(); it != va.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::cout << va.size() << std::endl;
+    // insert
+    std::cout << "insert" << std::endl;
+    va.insert(++va.begin(), 3);
+    va.insert(++va.begin(), 3);
+     for (Vector<int>::iterator it = va.begin(); it != va.end(); it++)
         std::cout << *it << std::endl;
-    std::cout << vo.capacity() << std::endl;
+    std::cout << va.capacity() << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    // std::vector<int> vo(10, 100);
+    // vo.push_back(4);
+    // vo.push_back(4);
+    // vo.pop_back();
+    // vo.assign(4, 5);
+    // vo.reserve(100);
+    // for (std::vector<int>::iterator it = vo.begin(); it != vo.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::cout << vo.capacity() << std::endl;
     std::vector<int> vop;
     vop.push_back(1);
     vop.push_back(2);
     vop.push_back(3);
+    vop.push_back(4);
+    // vop.push_back(5);
+    //  std::vector<int>::iterator too = vop.erase(vop.begin());
+    // for (std::vector<int>::iterator it = vop.begin(); it != vop.end(); it++)
+    //     std::cout << *it << std::endl;
+    // std::vector<int>::iterator tat = vop.erase(vop.begin(), vop.end());
+    // for (std::vector<int>::iterator it = vop.begin(); it != vop.end(); it++)
+    //     std::cout << *it << std::endl;
+        // std::cout << vop.capacity() << std::endl;
+    vop.insert(++vop.begin(),3);
+    vop.insert(++vop.begin(),3);
     for (std::vector<int>::iterator it = vop.begin(); it != vop.end(); it++)
         std::cout << *it << std::endl;
-    std::vector<int>::iterator ta = vop.erase(--vop.end());
-    std::vector<int>::iterator pa = vop.begin() - ta;
-    std::cout << *pa << std::endl;
-    // std::cout << vop.size() << std::endl;
-    while (1);
+    std::cout << vop.capacity() << std::endl;
     return (0);
 }
